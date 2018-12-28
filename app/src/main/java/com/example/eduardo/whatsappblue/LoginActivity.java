ae818b77dfa6371484dabadbd89ab8f6bf72010a
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2018-12-28 02:38:08 -0300

    creating login and register interface

---
+++ app/src/main/java/com/example/eduardo/whatsappblue/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.eduardo.whatsappblue;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void abrirTelaCadastro(View view){
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
