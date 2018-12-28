8ca23fac9c023c5dd7b35af6ceeba0f0359c44ed
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2018-12-28 03:07:10 -0300

    user registration

--- app/src/main/java/com/example/eduardo/whatsappblue/activity/LoginActivity.java
+++ app/src/main/java/com/example/eduardo/whatsappblue/activity/LoginActivity.java
@@ -1,9 +1,11 @@
-package com.example.eduardo.whatsappblue;
+package com.example.eduardo.whatsappblue.activity;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import com.example.eduardo.whatsappblue.R;
 
 public class LoginActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/eduardo/whatsappblue/activity/MainActivity.java
+++ app/src/main/java/com/example/eduardo/whatsappblue/activity/MainActivity.java
@@ -1,13 +1,15 @@
-package com.example.eduardo.whatsappblue;
+package com.example.eduardo.whatsappblue.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.example.eduardo.whatsappblue.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_main);
     }
 }
