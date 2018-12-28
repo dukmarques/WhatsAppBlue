6ff0ffbd40409a8e89f36d0da51398b1095d2133
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2018-12-28 03:46:57 -0300

    creating toolbar

--- app/src/main/java/com/example/eduardo/whatsappblue/activity/LoginActivity.java
+++ app/src/main/java/com/example/eduardo/whatsappblue/activity/LoginActivity.java
@@ -17,6 +17,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private TextInputEditText campoEmail, campoPassword;
@@ -77,6 +78,15 @@ public class LoginActivity extends AppCompatActivity {
             }
         }else{
             Toast.makeText(this, "Preencha o e-mail!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null){
+            openMainScreen();
         }
     }
 
