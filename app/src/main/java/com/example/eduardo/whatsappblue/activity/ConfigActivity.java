6798e16531b36a8d4f0575d821290e86527e1783
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2018-12-29 02:26:32 -0300

    recovering selected photo

--- app/src/main/java/com/example/eduardo/whatsappblue/activity/ConfigActivity.java
+++ app/src/main/java/com/example/eduardo/whatsappblue/activity/ConfigActivity.java
@@ -4,8 +4,11 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +19,8 @@ import android.widget.ImageButton;
 import com.example.eduardo.whatsappblue.R;
 import com.example.eduardo.whatsappblue.helper.Permissions;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ConfigActivity extends AppCompatActivity {
     private String[] requeridPermissions = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -24,6 +29,7 @@ public class ConfigActivity extends AppCompatActivity {
     private ImageButton imageButtonCamera, imageButtonGallery;
     private static final int SELECAO_CAMERA = 100;
     private static final int SELECAO_GALLERY = 200;
+    private CircleImageView circleImageViewPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,7 @@ public class ConfigActivity extends AppCompatActivity {
 
         imageButtonCamera = findViewById(R.id.imageButtonCamera);
         imageButtonGallery = findViewById(R.id.imageButtonGallery);
+        circleImageViewPerfil = findViewById(R.id.circleImageViewPhotoPerfil);
 
         Toolbar toolbar = findViewById(R.id.toolbarMain);
         toolbar.setTitle("Ajustes");
@@ -51,6 +58,44 @@ public class ConfigActivity extends AppCompatActivity {
                 }
             }
         });
+
+        imageButtonGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if (i.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(i, SELECAO_GALLERY);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            Bitmap image = null;
+
+            try{
+                switch (requestCode){
+                    case SELECAO_CAMERA:
+                        image = (Bitmap) data.getExtras().get("data");
+                        break;
+                    case SELECAO_GALLERY:
+                        Uri locateImageSelected = data.getData();
+                        image = MediaStore.Images.Media.getBitmap(getContentResolver(), locateImageSelected);
+                        break;
+                }
+
+                if (image != null){
+                    circleImageViewPerfil.setImageBitmap(image);
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
