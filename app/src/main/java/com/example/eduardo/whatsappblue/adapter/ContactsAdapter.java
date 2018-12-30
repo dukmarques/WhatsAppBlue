7a2a2e74ed148716ee44031ef3dec5cae289ea77
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2018-12-30 02:39:13 -0300

    listing contacts

---
+++ app/src/main/java/com/example/eduardo/whatsappblue/adapter/ContactsAdapter.java
@@ -0,0 +1,68 @@
+package com.example.eduardo.whatsappblue.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.eduardo.whatsappblue.R;
+import com.example.eduardo.whatsappblue.model.User;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyViewHolder> {
+    private List<User> contacts;
+    private Context context;
+
+    public ContactsAdapter(List<User> contactsList, Context c) {
+        this.contacts = contactsList;
+        this.context = c;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemList = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.adapter_contacts, viewGroup, false);
+        return new MyViewHolder(itemList);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        User user = contacts.get(i);
+
+        myViewHolder.name.setText(user.getName());
+        myViewHolder.email.setText(user.getEmail());
+
+        if (user.getPhoto() != null){
+            Uri uri = Uri.parse(user.getPhoto());
+            Glide.with(context).load(uri).into(myViewHolder.photo);
+        }else{
+            myViewHolder.photo.setImageResource(R.drawable.padrao);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return contacts.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        CircleImageView photo;
+        TextView name, email;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            photo = itemView.findViewById(R.id.imageViewPhotoContact);
+            name = itemView.findViewById(R.id.textNameContact);
+            email = itemView.findViewById(R.id.textEmailContact);
+        }
+    }
+}
--- app/src/main/java/com/example/eduardo/whatsappblue/fragment/ContactsFragment.java
+++ app/src/main/java/com/example/eduardo/whatsappblue/fragment/ContactsFragment.java
@@ -2,18 +2,34 @@ package com.example.eduardo.whatsappblue.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.eduardo.whatsappblue.R;
+import com.example.eduardo.whatsappblue.adapter.ContactsAdapter;
+import com.example.eduardo.whatsappblue.config.ConfigurationFirebase;
+import com.example.eduardo.whatsappblue.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContactsFragment extends Fragment {
-
+    private RecyclerView recyclerViewListContacts;
+    private ContactsAdapter adapter;
+    private ArrayList<User> contactsList = new ArrayList<>();
+    private DatabaseReference usersRef;
+    private ValueEventListener valueEventListenerContacts;
 
     public ContactsFragment() {
         // Required empty public constructor
@@ -24,7 +40,54 @@ public class ContactsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        //Initial Settings
+        recyclerViewListContacts = view.findViewById(R.id.recyclerViewListContacts);
+        usersRef = ConfigurationFirebase.getFirebaseDatabase().child("usuarios");
+
+        //Configure adapter
+        adapter = new ContactsAdapter(contactsList, getActivity());
+
+        //Configure recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewListContacts.setLayoutManager(layoutManager);
+        recyclerViewListContacts.setHasFixedSize(true);
+        recyclerViewListContacts.setAdapter(adapter);
+
+        return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recoveringContacts();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        usersRef.removeEventListener(valueEventListenerContacts);
+    }
+
+    public void recoveringContacts(){
+        valueEventListenerContacts = usersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dados : dataSnapshot.getChildren()){
+                    User user = dados.getValue(User.class);
+                    contactsList.add(user);
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 
 }
