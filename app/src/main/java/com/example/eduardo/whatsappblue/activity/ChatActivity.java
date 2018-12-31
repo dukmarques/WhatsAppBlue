b19a4547cf149329171e82f5ce76bb5233862097
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2018-12-31 03:09:26 -0300

    listing messages

--- app/src/main/java/com/example/eduardo/whatsappblue/activity/ChatActivity.java
+++ app/src/main/java/com/example/eduardo/whatsappblue/activity/ChatActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
@@ -13,12 +15,16 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.eduardo.whatsappblue.R;
+import com.example.eduardo.whatsappblue.adapter.MessagesAdapter;
 import com.example.eduardo.whatsappblue.config.ConfigurationFirebase;
 import com.example.eduardo.whatsappblue.helper.Base64Custom;
 import com.example.eduardo.whatsappblue.helper.UserFirebase;
 import com.example.eduardo.whatsappblue.model.Message;
 import com.example.eduardo.whatsappblue.model.User;
 import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -31,6 +37,10 @@ public class ChatActivity extends AppCompatActivity {
     //Sender and recipient user identifier
     private String idUserRecipient;
     private String idUserSender;
+
+    private RecyclerView recyclerMessages;
+    private MessagesAdapter adapter;
+    private List<Message> messages = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +58,7 @@ public class ChatActivity extends AppCompatActivity {
         textViewNome = findViewById(R.id.textViewNameChat);
         circleImageViewPhoto = findViewById(R.id.circleImagePhotoChat);
         editMessage = findViewById(R.id.editMessage);
+        recyclerMessages = findViewById(R.id.recyclerMessages);
 
         //Recover user data from sender
         idUserSender = UserFirebase.getIdUser();
@@ -71,6 +82,15 @@ public class ChatActivity extends AppCompatActivity {
             //recover user data from recipient
             idUserRecipient = Base64Custom.encodingBase64(recipientUser.getEmail());
         }
+
+        //Configurind Adapter
+        adapter = new MessagesAdapter(messages, getApplicationContext());
+
+        //Configuring recyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerMessages.setLayoutManager(layoutManager);
+        recyclerMessages.setHasFixedSize(true);
+        recyclerMessages.setAdapter(adapter);
     }
 
     public void sendMessage(View view){
