05b92cf0343f5112f7787dafe4b2721426c4a49f
dukmarques <eduardomarques@outlook.com>
2018-12-31 18:34:54 -0300

    complete about page

--- app/src/main/java/com/example/eduardo/whatsappblue/activity/AboutActivity.java
+++ app/src/main/java/com/example/eduardo/whatsappblue/activity/AboutActivity.java
@@ -13,13 +13,26 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_about);
+
+        String description = "Este app foi feito com fins educacionais e não possui nenhuma relação, conexão ou " +
+                "ligação com o WhatsApp." +
+                "\nEste não é um aplicativo oficial!\n"+
+                "\nEste app foi desenvolvido por Eduardo Marques, estudante de " +
+                "Engenharia de Computação da UEFS em seu estudo de programação para Android."+
+                "\n Nenhum de seus dados serão utilizados fora deste App (Infelizmente :D).";
 
         View aboutPage = new AboutPage(this)
                 .setImage(R.drawable.logo_blue)
-                .addGroup("Sobre o App")
+                .setDescription(description)
+
+                .addGroup("Sobre Eduardo Marques")
+                .addEmail("eduardomarques@outlook.com","Envie um e-mail")
+                .addGitHub("dukmarques", "GitHub")
+                .addFacebook("duduumarques", "Facebook")
+                .addInstagram("dudu.ms", "Instagram")
+                .addTwitter("duk_ms", "Twitter")
                 .create();
-        
+
         setContentView(aboutPage);
     }
 }
