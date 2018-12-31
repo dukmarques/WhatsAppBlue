4d80f712d93a49f4f0629f70bd58498415996eee
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2018-12-31 02:44:47 -0300

    saving message

--- app/src/main/java/com/example/eduardo/whatsappblue/activity/ChatActivity.java
+++ app/src/main/java/com/example/eduardo/whatsappblue/activity/ChatActivity.java
@@ -7,18 +7,30 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.eduardo.whatsappblue.R;
+import com.example.eduardo.whatsappblue.config.ConfigurationFirebase;
+import com.example.eduardo.whatsappblue.helper.Base64Custom;
+import com.example.eduardo.whatsappblue.helper.UserFirebase;
+import com.example.eduardo.whatsappblue.model.Message;
 import com.example.eduardo.whatsappblue.model.User;
+import com.google.firebase.database.DatabaseReference;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
     private TextView textViewNome;
     private CircleImageView circleImageViewPhoto;
+    private EditText editMessage;
     private User recipientUser;
+
+    //Sender and recipient user identifier
+    private String idUserRecipient;
+    private String idUserSender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +47,10 @@ public class ChatActivity extends AppCompatActivity {
         //Initial settings
         textViewNome = findViewById(R.id.textViewNameChat);
         circleImageViewPhoto = findViewById(R.id.circleImagePhotoChat);
+        editMessage = findViewById(R.id.editMessage);
+
+        //Recover user data from sender
+        idUserSender = UserFirebase.getIdUser();
 
         //Recover user data from recipient
         Bundle bundle = getIntent().getExtras();
@@ -51,7 +67,36 @@ public class ChatActivity extends AppCompatActivity {
             }else{
                 circleImageViewPhoto.setImageResource(R.drawable.padrao);
             }
+
+            //recover user data from recipient
+            idUserRecipient = Base64Custom.encodingBase64(recipientUser.getEmail());
         }
     }
 
+    public void sendMessage(View view){
+        String textMessage = editMessage.getText().toString();
+        if (!textMessage.isEmpty()){
+            Message message = new Message();
+            message.setIdUser(idUserSender);
+            message.setMessage(textMessage);
+
+            //save message from sender
+            saveMessage(idUserSender, idUserRecipient, message);
+        }else{
+            Toast.makeText(this, "Digite uma mensagem para enviar", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void saveMessage(String idSender, String idRecipient, Message msg){
+        DatabaseReference database = ConfigurationFirebase.getFirebaseDatabase();
+        DatabaseReference messageRef = database.child("mensagens");
+
+        messageRef.child(idSender)
+                .child(idRecipient)
+                .push()
+                .setValue(msg);
+
+        //clear text
+        editMessage.setText("");
+    }
 }
---
+++ app/src/main/java/com/example/eduardo/whatsappblue/model/Message.java
@@ -0,0 +1,34 @@
+package com.example.eduardo.whatsappblue.model;
+
+public class Message {
+    private String idUser;
+    private String message;
+    private String image;
+
+    public Message() {
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
