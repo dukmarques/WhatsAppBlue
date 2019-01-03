f461130abbe0b3d243e72721fafe66ae6639e341
Eduardo Marques <33849830+dukmarques@users.noreply.github.com>
2019-01-03 01:10:35 -0300

    Creating group registration interface

--- app/src/main/java/com/example/eduardo/whatsappblue/activity/GroupRegistrationActivity.java
+++ app/src/main/java/com/example/eduardo/whatsappblue/activity/GroupRegistrationActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.eduardo.whatsappblue.R;
+import com.example.eduardo.whatsappblue.adapter.GroupSelectedAdapter;
 import com.example.eduardo.whatsappblue.model.User;
 
 import java.util.ArrayList;
@@ -16,13 +19,17 @@ import java.util.List;
 
 public class GroupRegistrationActivity extends AppCompatActivity {
     private List<User> selectedMembersList = new ArrayList<>();
-    private TextView textTotalMembers;
+    private TextView textTotalParticipants;
+    private GroupSelectedAdapter groupSelectedAdapter;
+    private RecyclerView recyclerSelectedMembers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_registration);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Novo grupo");
+        toolbar.setSubtitle("Defina o nome");
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -36,15 +43,29 @@ public class GroupRegistrationActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Initial settings
-        textTotalMembers = findViewById(R.id.textTotal);
+        textTotalParticipants = findViewById(R.id.textTotalParticipants);
+        recyclerSelectedMembers = findViewById(R.id.recyclerMembersGroup);
 
         //Retrieve past member list
         if (getIntent().getExtras() != null){
             List<User> members = (List<User>) getIntent().getExtras().getSerializable("members");
             selectedMembersList.addAll(members);
 
-            textTotalMembers.setText("Total: " + selectedMembersList.size());
-        }
-    }
+            textTotalParticipants.setText("Participantes: " + selectedMembersList.size());
 
+        }
+
+        //Configure adapter
+        groupSelectedAdapter = new GroupSelectedAdapter(selectedMembersList, getApplicationContext());
+
+        //Configure recyclerView
+        RecyclerView.LayoutManager layoutManagerHorizontal = new LinearLayoutManager(
+                getApplicationContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false
+        );
+        recyclerSelectedMembers.setLayoutManager(layoutManagerHorizontal);
+        recyclerSelectedMembers.setHasFixedSize(true);
+        recyclerSelectedMembers.setAdapter(groupSelectedAdapter);
+    }
 }
